247e522e6a3f00ce3cfce4e1cf97bd2f044f40df
Samara Oliveira <samara.081007@alunos.utfpr.edu.br>
2026-04-15 10:59:54 -0300

    start-objetos

---
+++ Aula-04-15/src/routes/tarefa.routes.js
@@ -0,0 +1,55 @@
+// @file: src/routes/tarefa.routes.js
+
+// Adicionar o import:
+import {
+  listarTarefas,
+  criarTarefa,
+  obterResumo,
+  obterTarefa,
+  atualizarTarefa,
+  concluirTarefa,
+  removerTarefa,
+  listarPendentes  
+} from '../controllers/tarefa.controller.js'
+
+
+export default async function tarefaRoutes(server, options) {
+
+  server.get('/tarefas', async (request, reply) => {
+    console.log("Routes: GET /tarefas chamada")
+    listarTarefas(request, reply)
+  })
+
+  server.post('/tarefas', async (request, reply) => {
+    console.log("Routes: POST /tarefas chamada")
+    criarTarefa(request, reply)
+  })
+
+  server.get('/tarefas/resumo', async (request, reply) => {
+    console.log("Routes: GET /tarefas/resumo chamada")
+    obterResumo(request, reply)
+  })
+
+  server.get('/tarefas/:id', async (request, reply) => {
+    console.log("Routes: GET /tarefas/:id chamada")
+    obterTarefa(request, reply)
+  })
+  server.get('/tarefas/pendentes', async (request, reply) => {
+  console.log("Routes: GET /tarefas/pendentes chamada")
+  listarPendentes(request, reply)
+})
+  server.patch('/tarefas/:id', async (request, reply) => {
+    console.log("Routes: PATCH /tarefas/:id chamada")
+    atualizarTarefa(request, reply)
+  })
+
+  server.patch('/tarefas/:id/concluir', async (request, reply) => {
+    console.log("Routes: PATCH /tarefas/:id/concluir chamada")
+    concluirTarefa(request, reply)
+  })
+
+  server.delete('/tarefas/:id', async (request, reply) => {
+    console.log("Routes: DELETE /tarefas/:id chamada")
+    removerTarefa(request, reply)
+  })
+}
